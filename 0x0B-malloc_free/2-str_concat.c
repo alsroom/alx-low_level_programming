85749e087ad71836da87bd6cbc5768fa839d3af2
Amin T <t4temitope@gmail.com>
2023-04-12 17:52:17 -0700

    update

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,19 +9,21 @@
  */
 
 char *str_concat(char *s1, char *s2)
-
 {
 	char *conct;
 	int i, ci;
 
 	if (s1 == NULL)
+	{
 		s1 = "";
+	}
 
 	if (s2 == NULL)
+	{
 		s2 = "";
+	}
 
-		i = ci = 0;
-
+	i = ci = 0;
 	while (s1[i] != '\0')
 		i++;
 
@@ -46,8 +48,6 @@ char *str_concat(char *s1, char *s2)
 		conct[i] = s2[ci];
 		i++, ci++;
 	}
-
 	conct[i] = '\0';
-
 	return (conct);
 }
